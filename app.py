d1fd9a1e936cb54ec484ee6db60a567f42ef3b80
Brian <brian.salkas@gmail.com>
2023-04-20 18:49:19 -0500

    no clear input

--- app.py
+++ app.py
@@ -2,20 +2,25 @@ import streamlit as st
 import create_jsonl as cj
 from LineObject import LineObject
 
+def clear_jsonl():
+    st.session_state['l'] = l = []
 st.title('Create JSON files to Fine-Tune OpenAI Models')
 l = []
 c1,c2 = st.columns([4,1],gap='large')
 if 'l' not in st.session_state:
     st.session_state['l'] = l
-l = ['first']
 with c1:
     prompt = st.text_input('**PROMPT**')
     completion = st.text_input('**COMPLETION**')
     if st.button('append line'):
-        st.session_state['l'].append(LineObject(prompt,completion))
+        if prompt == "" or completion == "":
+            st.markdown(':red[**please do not leave promp/completion blank**]') 
+        else:
+            st.session_state['l'].append(LineObject(prompt,completion))
 with c2:
     PEND = st.text_input('prompt end')
     CEND = st.text_input('completion end')
+    st.button('clear JSONL', on_click=clear_jsonl)
 
 if not PEND:
     st.markdown(r':red[it is recommended that you use a prompt end such as \n\n###\n\n]')
